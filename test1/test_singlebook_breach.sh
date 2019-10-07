811d51748326418578ed075a38d35f3f81c0a261
Aimoux <oceanambition@163.com>
2019-10-07 20:03:19 +0800

    add breach

---
+++ test1/test_singlebook_breach.sh
@@ -0,0 +1,3 @@
+python ../src/excel_and_json.py singlebook -o ./ -i Weapons.xlsx
+python ../src/excel_and_json.py singlebook -o ./ -i Roles.xlsx
+python ../src/excel_and_json.py singlebook -o ./ -i RoleAttributes.xlsx

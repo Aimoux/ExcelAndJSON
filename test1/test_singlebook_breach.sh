e845aa8e7f2a46223662dffbf8d26370b6800401
Aimoux <oceanambition@163.com>
2019-10-12 15:53:50 +0800

    Add Weapon Data

--- test1/test_singlebook_breach.sh
+++ test1/test_singlebook_breach.sh
@@ -1,3 +1,5 @@
 python ../src/excel_and_json.py singlebook -o ./ -i Weapons.xlsx
 python ../src/excel_and_json.py singlebook -o ./ -i Roles.xlsx
 python ../src/excel_and_json.py singlebook -o ./ -i RoleAttributes.xlsx
+python ../src/excel_and_json.py singlebook -o ./ -i WeaponAdds.xlsx
+python ../src/excel_and_json.py singlebook -o ./ -i WeaponAttributes.xlsx

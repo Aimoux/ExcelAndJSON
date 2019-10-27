7f70bd981ad5e7aaf60fa6ff943c70a860d44a81
Aimoux <oceanambition@163.com>
2019-10-27 23:59:21 +0800

    Fix Monster Add Animation

--- test1/test_singlebook_breach.sh
+++ test1/test_singlebook_breach.sh
@@ -3,7 +3,9 @@ python ../src/excel_and_json.py singlebook -o ./ -i Weapons.xlsx
 python ../src/excel_and_json.py singlebook -o ./ -i WeaponAdds.xlsx
 python ../src/excel_and_json.py singlebook -o ./ -i WeaponAttributes.xlsx
 python ../src/excel_and_json.py singlebook -o ./ -i WeaponFrames.xlsx
+python ../src/excel_and_json.py singlebook -o ./ -i WeaponImpacts.xlsx
 python ../src/excel_and_json.py singlebook -o ./ -i Roles.xlsx
 python ../src/excel_and_json.py singlebook -o ./ -i RoleAttributes.xlsx
 python ../src/excel_and_json.py singlebook -o ./ -i LevelMonsters.xlsx
 python ../src/excel_and_json.py singlebook -o ./ -i Monsters.xlsx
+python ../src/excel_and_json.py singlebook -o ./ -i Animations.xlsx

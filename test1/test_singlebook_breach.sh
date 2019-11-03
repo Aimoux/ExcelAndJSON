e81fe878cabccd077d51552de4479b2a7e84f01b
Aimoux <oceanambition@163.com>
2019-11-03 22:33:29 +0800

    Add Transit

--- test1/test_singlebook_breach.sh
+++ test1/test_singlebook_breach.sh
@@ -9,3 +9,4 @@ python ../src/excel_and_json.py singlebook -o ./ -i RoleAttributes.xlsx
 python ../src/excel_and_json.py singlebook -o ./ -i LevelMonsters.xlsx
 python ../src/excel_and_json.py singlebook -o ./ -i Monsters.xlsx
 python ../src/excel_and_json.py singlebook -o ./ -i Animations.xlsx
+python ../src/excel_and_json.py singlebook -o ./ -i Transits.xlsx
